bb28e74ff1f437406f332bea386dff5f6f9b60d3
Kawtious <mewmewby2@gmail.com>
2022-04-27 16:59:24 -0700

    Added project files

---
+++ app/src/main/java/jose/cazarez/firebaselogin/MainActivity.kt
@@ -0,0 +1,22 @@
+package jose.cazarez.firebaselogin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import jose.cazarez.firebaselogin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+    }
+}
---
+++ app/src/main/java/jose/cazarez/firebaselogin/SignInActivity.kt
@@ -0,0 +1,66 @@
+package jose.cazarez.firebaselogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import jose.cazarez.firebaselogin.databinding.ActivitySignInBinding
+
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_sign_in)
+
+        auth = Firebase.auth
+
+        binding.signInAppCompatButton.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            when {
+                mEmail.isEmpty() || mPassword.isEmpty() -> {
+                    Toast.makeText(
+                        baseContext, "Mail o contrasena incorrecta.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    SignIn(mEmail, mPassword)
+                }
+            }
+        }
+    }
+
+    private fun SignIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    reload()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun reload() {
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+}
